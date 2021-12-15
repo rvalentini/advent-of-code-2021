3d0b3456ed8c5fb0cf76215bdb22e901c0dbbb30
rvalentini <ri.valentini@reply.de>
2021-12-15 16:15:18 +0100

    Day 3 - Part 2 complete

--- src/rvalentini/day_3.clj
+++ src/rvalentini/day_3.clj
@@ -17,13 +17,42 @@
   [bit-counts eval-fn]
   (bin->dec (apply str (map eval-fn bit-counts))))
 
+(defn filter-bit-at-pos
+  [report bit pos]
+  (filter #(= (nth % pos) bit) report))
+
+(defn calculate-bit-counts
+  [report]
+  (map #(reduce bit-count [0 0] %) (apply mapv vector report)))
+
+(defn oxygen-eval [[zeros ones]] (if (> zeros ones) \0 \1))
+
+(defn c02-eval [[zeros ones]] (if (> zeros ones) \1 \0))
+
+(defn calculate-rating
+  [report eval-fn]
+  (loop [report report
+         bit-counts (calculate-bit-counts report)
+         i 0]
+    (if (= (count report) 1)
+      (flatten report)
+      (let [filtered (filter-bit-at-pos report (eval-fn (nth bit-counts i)) i)
+            bit-counts (calculate-bit-counts filtered)]
+        (recur filtered bit-counts (inc i))))))
+
 (comment
   (with-open [rdr (io/reader (io/resource "day_3_input.txt"))]
-    (let [bit-counts (map #(reduce bit-count [0 0] %) (apply mapv vector (map seq (line-seq rdr))))
+    (let [report (map seq (line-seq rdr))
+          bit-counts (calculate-bit-counts report)
           gamma-rate (to-rate bit-counts gamma-eval)
           epsilon-rate (to-rate bit-counts epsilon-eval)]
       (* gamma-rate epsilon-rate)))
 
+  (with-open [rdr (io/reader (io/resource "day_3_input.txt"))]
+    (let [report (map seq (line-seq rdr))
+          oxygen-rating (bin->dec (apply str (calculate-rating report oxygen-eval)))
+          c02-rating (bin->dec (apply str (calculate-rating report c02-eval)))]
+      (* oxygen-rating c02-rating )))
   )
 
 
