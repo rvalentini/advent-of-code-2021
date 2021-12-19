46784fd52dea483890cfa7bca2bd357e38131824
rvalentini <ri.valentini@reply.de>
2021-12-19 16:51:03 +0100

    Day 4 - Part 1 complete

--- src/rvalentini/day_4.clj
+++ src/rvalentini/day_4.clj
@@ -3,10 +3,8 @@
             [clojure.string :as str]
             [clojure.pprint :refer [pprint]]))
 
-;; DONT USE SPLIT AT but transform to vec and then use assoc
 (defn mark-at-pos
-  [board row col]
-  (println "BOARD IN MARK POS: " board)
+  [[row col] board]
   (if (and row col)
     (let [[r1 r2] (split-at row board)
           [c1 c2] (split-at col (first r2))]
@@ -15,21 +13,23 @@
         (drop 1 r2)))
     board))
 
-(defn mark-number
+(defn get-pos-for-number
   [board n]
-  (println "BOARD IN MARK NUMBER: " board)
-  (let [[row col _] (first (->> (for [[x row] (map-indexed vector board)
-                                      [y val] (map-indexed vector row)]
-                                  [x y val])
-                             (filter #(= (:val (nth % 2)) n))))]
-    (mark-at-pos board row col)))
+  (first (->> (for [[x row] (map-indexed vector board)
+                    [y val] (map-indexed vector row)]
+                [[x y] val])
+           (filter #(= (:val (second %)) n))
+           (map first))))
 
+(defn all-marked?
+  [seq]
+  (every? #(true? (:marked %)) seq))
 
 (defn is-winner?
   [board]
   (or
-    (some true? (->> board (map (fn [row] (every? #(true? (:marked %)) row)))))
-    (some true? (->> (apply mapv vector board) (map (fn [col] (every? #(true? (:marked %)) col)))))))
+    (some true? (->> board (map all-marked?)))
+    (some true? (->> (apply mapv vector board) (map all-marked?)))))
 
 (defn print-pretty [board]
   (pprint (for [row board]
@@ -38,11 +38,18 @@
                 (str "*" (:val n) "*")
                 (:val n))))))
 
+(defn calculate-score
+  [board]
+  (reduce + 0 (->> (flatten board)
+                (filter #(false? (:marked %)))
+                (map #(Integer/parseInt (:val %) 10)))))
+
 
 (defn number-reducer
   [boards n]
-  (println "BOARDS" boards)
-  (let [marked-boards (map #(mark-number % n) boards)
+  (let [marked-boards (map #(-> %
+                              (get-pos-for-number n)
+                              (mark-at-pos %)) boards)
         winner (first (filter is-winner? marked-boards))]
     (if (some? winner)
       (reduced [n winner])
@@ -55,37 +62,11 @@
           boards (partition 5 (->> rest
                                 (filter #(not= % ""))
                                 (map #(str/split % #"\s+"))
-                                (map #(map (fn [n] {:val n :marked false}) %))))]
-      ;(println numbers)
-      ;(println (second numbers))
+                                (map #(map (fn [n] {:val n :marked false}) %))))
+          [n winner] (reduce number-reducer boards numbers)]
 
-
-      (print-pretty  (mark-number (first boards) "61"))
-      (let [[n winner] (reduce number-reducer boards numbers)]
-        (println "NUMBER:" n)
-        (print-pretty winner)
-        )
-
-
-
-      ;(println boards)
-      ;(println (is-winner? (first boards)))
-
-
-      #_(let [board (first boards)
-            winner (-> (first boards)
-                     (mark-at-pos 0 2)
-                     (mark-at-pos 1 2)
-                     (mark-at-pos 2 2)
-                     (mark-at-pos 3 2)
-                     (mark-at-pos 4 2))]
-
-        (print-pretty board)
-        (println "IS WINNER? " (is-winner? board))
-        (print-pretty winner)
-        (println "IS WINNER? " (is-winner? winner)))
-      ))
-
+      (print-pretty winner)
+      (println "Winner score:" (* (Integer/parseInt n 10) (calculate-score winner)))))
   )
 
 
