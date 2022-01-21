ebf4dbad0ef2aadec550a40dd98b1e80a092a455
rvalentini <ri.valentini@reply.de>
2022-01-21 14:20:28 +0100

    Day 14 - Part 2 complete

--- src/rvalentini/day_14.clj
+++ src/rvalentini/day_14.clj
@@ -2,33 +2,52 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as s]))
 
-(defn insert
-  [pair element]
-  (str (first pair) element (second pair)))
+(defn to-pair-map
+  [template]
+  (->> template
+    (map str)
+    ((fn [l] [l (rest l)]))
+    ((fn [[a b]] (map str a b)))
+    (map #(identity {% 1}))
+    (into {})))
 
-(defn match
-  [pairs [pair target]]
-  (map #(if (= % pair) (insert % target) %) pairs))
+(defn splice
+  [pair target]
+  [(str (first pair) target)
+   (str target (last pair))])
 
-(defn merge-pairs
-  [result [_ & rest]]
-  (str result (apply str rest)))
+(defn splice-update [state [pair cnt] target ]
+  (let [[p1 p2] (splice pair target)]
+    (-> state
+      (update pair #(- (or % 0) cnt))
+      (update p1 #(+ (or % 0) cnt))
+      (update p2 #(+ (or % 0) cnt)))))
 
-(defn apply-rules
-  [template rules]
-  (let [pairs (map #(apply str %) (partition 2 1 template))
-        [first & rest] (reduce match pairs rules)]
-    (reduce merge-pairs first rest)))
+(defn splice-reducer
+  [[state rules] element]
+  [(if-let [target (rules (first element) )]
+     (into {} (filter #(-> % val pos?) (splice-update state element target)))
+     state) rules])
+
+(defn first-char-reducer
+  [agg [pair cnt]]
+  (update agg (str (first pair)) #(+ (or % 0) cnt)))
+
+(defn to-char-counts
+  [state]
+  (reduce first-char-reducer {} (seq state)))
+
+(defn step
+  [[state rules]]
+  (reduce splice-reducer [state rules] (seq state)))
 
 (defn str->rule
   [str]
   (map #(s/trim %) (s/split str #"->")))
 
-(defn element-frequency
-  [counts element]
-  (if (contains? counts element)
-    (update counts element inc)
-    (assoc counts element 1)))
+(defn most-frequent [state] (reduce max (map val (seq state))))
+
+(defn least-frequent [state] (reduce min (map val (seq state))))
 
 (comment
   (with-open [rdr (io/reader (io/resource "day_14_input.txt"))]
@@ -36,14 +55,12 @@
                     (partition-by #(empty? %))
                     (filter #(not= (first %) "")))
           template (first s1)
-          rules (map str->rule s2)
-          step-fn (iterate #(apply-rules % rules) template)
-          result (last (take 11 step-fn))
-          frequencies (reduce element-frequency {} result)
-          highest (second (first (sort-by val > frequencies)))
-          lowest (second (last (sort-by val > frequencies)))]
-
-      (println "Highest - lowest element frequency after 10 iterations: "
-        (- highest lowest))))
+          rules (into {} (map vec (map str->rule s2)))
+          state (to-pair-map template)
+          last-char (last template)
+          [final-state _] (last (take 41 (iterate step [state rules])))
+          counts (update (to-char-counts final-state) (str last-char) inc)]
+      (println "Difference between most frequent and least frequent char counts: "
+        (- (most-frequent counts) (least-frequent counts)))))
   )
 
